73bf6ccc24f57751059d70f84c047b4f481d8d80
Sardiko <55611350+oki20@users.noreply.github.com>
2023-04-02 16:00:04 +0700

    Test
    Perbaikan pada assets

--- content_index/index_admin.php
+++ content_index/index_admin.php
@@ -1,50 +1,56 @@
-<?php 
-	session_start();
-	include('../koneksi.php');
-	// cek apakah yang mengakses halaman ini sudah login
-	if($_SESSION['role_user']==""){
-		header("location:index.php?pesan=gagal");
-	}
- 
+<?php
+session_start();
+include('../koneksi.php');
+// cek apakah yang mengakses halaman ini sudah login
+if ($_SESSION['role_user'] == "") {
+	header("location:index.php?pesan=gagal");
+}
+
 ?>
 
 <!-- menghitung jumlah data -->
 <?php
-	// Query untuk menghitung jumlah data akun
-	$akun = "SELECT COUNT(*) as jumlah_akun FROM user";
-	$jumlah_akun = mysqli_query($koneksi, $akun);
-	$data_akun = mysqli_fetch_assoc($jumlah_akun);
+// Query untuk menghitung jumlah data akun
+$akun = "SELECT COUNT(*) as jumlah_akun FROM user";
+$jumlah_akun = mysqli_query($koneksi, $akun);
+$data_akun = mysqli_fetch_assoc($jumlah_akun);
 
-	// Query untuk menghitung jumlah data siswa
-	$siswa = "SELECT COUNT(*) as jumlah_siswa FROM tabel_siswa";
-	$jumlah_siswa = mysqli_query($koneksi, $siswa);
-	$data_siswa = mysqli_fetch_assoc($jumlah_siswa);
-	
-	// Query untuk menghitung jumlah data mapel
-	$mapel = "SELECT COUNT(*) as jumlah_mapel FROM tabel_mapel";
-	$jumlah_mapel = mysqli_query($koneksi, $mapel);
-	$data_mapel = mysqli_fetch_assoc($jumlah_mapel);
+// Query untuk menghitung jumlah data siswa
+$siswa = "SELECT COUNT(*) as jumlah_siswa FROM tabel_siswa";
+$jumlah_siswa = mysqli_query($koneksi, $siswa);
+$data_siswa = mysqli_fetch_assoc($jumlah_siswa);
 
-	// Query untuk menghitung jumlah data guru
-	$guru = "SELECT COUNT(*) as jumlah_guru FROM user WHERE role_user = 'guru' ";
-	$jumlah_guru = mysqli_query($koneksi, $guru);
-	$data_guru = mysqli_fetch_assoc($jumlah_guru);
+// Query untuk menghitung jumlah data mapel
+$mapel = "SELECT COUNT(*) as jumlah_mapel FROM tabel_mapel";
+$jumlah_mapel = mysqli_query($koneksi, $mapel);
+$data_mapel = mysqli_fetch_assoc($jumlah_mapel);
+
+// Query untuk menghitung jumlah data guru
+$guru = "SELECT COUNT(*) as jumlah_guru FROM user WHERE role_user = 'guru' ";
+$jumlah_guru = mysqli_query($koneksi, $guru);
+$data_guru = mysqli_fetch_assoc($jumlah_guru);
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	<title>Dashboard Admin</title>
 	<meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
-	<link rel="icon" href="../../assets/img/icon.ico" type="image/x-icon"/>
+	<link rel="icon" href="../../assets/img/icon.ico" type="image/x-icon" />
 
 	<!-- Fonts and icons -->
-	<script src="../../assets/js/plugin/webfont/webfont.min.js"></script>
+	<script src="../assets/js/plugin/webfont/webfont.min.js"></script>
 	<script>
 		WebFont.load({
-			google: {"families":["Lato:300,400,700,900"]},
-			custom: {"families":["Flaticon", "Font Awesome 5 Solid", "Font Awesome 5 Regular", "Font Awesome 5 Brands", "simple-line-icons"], urls: ['../assets/css/fonts.min.css']},
+			google: {
+				"families": ["Lato:300,400,700,900"]
+			},
+			custom: {
+				"families": ["Flaticon", "Font Awesome 5 Solid", "Font Awesome 5 Regular", "Font Awesome 5 Brands", "simple-line-icons"],
+				urls: ['../assets/css/fonts.min.css']
+			},
 			active: function() {
 				sessionStorage.fonts = true;
 			}
@@ -52,23 +58,23 @@
 	</script>
 
 	<!-- CSS Files -->
-	<link rel="stylesheet" href="../../assets/css/bootstrap.min.css">
-	<link rel="stylesheet" href="../../assets/css/atlantis.min.css">
+	<link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="../assets/css/atlantis.min.css">
 
 	<!-- CSS Just for demo purpose, don't include it in your project -->
-	<link rel="stylesheet" href="../../assets/css/demo.css">
+	<link rel="stylesheet" href="../assets/css/demo.css">
 </head>
 
 <body>
 	<div class="wrapper">
 		<div class="main-header">
 			<?php
-				include('includes/../../layout/navbar.php');
+			include('includes/../../layout/navbar.php');
 			?>
 		</div>
 
 		<!-- Sidebar -->
-		<div class="sidebar sidebar-style-2">			
+		<div class="sidebar sidebar-style-2">
 			<div class="sidebar-wrapper scrollbar scrollbar-inner">
 				<div class="sidebar-content">
 					<div class="user">
@@ -78,9 +84,9 @@
 						<div class="info">
 							<a data-toggle="collapse" href="#collapseExample" aria-expanded="true">
 								<span>
-									<?php echo $_SESSION['username'];?>
-								<span class="user-level"><?php echo $_SESSION['role_user'];?></span>
-								<span class="caret"></span>
+									<?php echo $_SESSION['username']; ?>
+									<span class="user-level"><?php echo $_SESSION['role_user']; ?></span>
+									<span class="caret"></span>
 								</span>
 							</a>
 							<div class="clearfix"></div>
@@ -142,7 +148,7 @@
 									</li>
 								</ul>
 							</div>
-						</li>	
+						</li>
 
 						<li class="nav-item">
 							<a href="menu_admin/data_mata_pelajaran.php">
@@ -168,7 +174,7 @@
 				<!-- header -->
 				<div class="panel-header bg-primary-gradient">
 					<?php
-						include('includes/../../layout/header.php');
+					include('includes/../../layout/header.php');
 					?>
 				</div>
 				<!-- end header -->
@@ -259,10 +265,10 @@
 									</div>
 									<div class="card-title">Selamat Datang <?php echo $_SESSION['username']; ?></div>
 									<div class="card-category">Anda bisa mengelola data user, kelas dan juga mata pelajaran.
-										
+
 									</div>
 									<div class="d-flex flex-wrap justify-content-around pb-2 pt-4">
-										
+
 									</div>
 								</div>
 							</div>
@@ -273,7 +279,7 @@
 			</div>
 			<footer class="footer">
 				<?php
-					include('includes/../../layout/footer.php');
+				include('includes/../../layout/footer.php');
 				?>
 			</footer>
 		</div>
@@ -311,4 +317,5 @@
 	<script src="../assets/js/atlantis.min.js"></script>
 
 </body>
+
 </html>
